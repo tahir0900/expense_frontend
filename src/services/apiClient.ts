41751e3fed011a3de4c3818f0bf565263220dd5d
tahir0900 <tahirafridi0900@gmail.com>
2025-12-20 02:56:10 +0500

    make two urls for local and live testing

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -2,7 +2,8 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const API_BASE_URL = "http://127.0.0.1:8000/api/";
+// const API_BASE_URL = "http://127.0.0.1:8000/api/";
+const API_BASE_URL = "https://expense-backend-1z0c.onrender.com/api/";
 
 export const TOKEN_COOKIE_KEY = "auth_token";
 
